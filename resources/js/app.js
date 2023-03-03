709c2b9b46ec2a4264aa4a819c972ceba21ad510
Jales Cardoso <jlscrds@gmail.com>
2023-03-03 15:21:46 -0300

    fix erro resouce css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
 import './bootstrap';
-
+import '../css/app.css'; 
 import jQuery from 'jquery';
 window.$ = jQuery;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Laravel</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
